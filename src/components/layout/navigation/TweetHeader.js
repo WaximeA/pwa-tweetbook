2d29cdab6bd89ddd57d3a479523a50170bd85a8d
Maxime AUBLET <maximeaublet@gmail.com>
2019-07-20 20:13:47 +0200

    UI v2

--- src/components/layout/navigation/TweetHeader.js
+++ src/components/layout/navigation/TweetHeader.js
@@ -1,42 +1,45 @@
-import {LitElement, html, css} from 'lit-element/lit-element';
+import { LitElement, html, css } from "lit-element/lit-element";
 
-import './TweetSidebar';
+import "./TweetSidebar";
 
 export default class TweetHeader extends LitElement {
   static get styles() {
     return css`
       header {
-        height: 48px;
-        display: block;
-        border-bottom: solid #eeeeee 1px;
-        box-shadow: #aaaaaa 0px 3px 4px 0px
+        height: 50px;
+        padding: 3px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        background-color: var(--app-bg-component-color);
+        border-bottom: solid var(--app-header-shadow) 1px;
+        box-shadow: var(--app-header-shadow) 0px 3px 4px 0px;
       }
-      
+
       .brand {
-        display: inline-block;
         margin: auto;
-        width: 70%;
-        text-align: center;
+        display: flex;
+        margin-left: 72px;
+        flex-direction: row;
       }
-      
+
       .brand img {
-        display: inline-block;
-        vertical-align: text-bottom;
-        height: 42px;
-        width: 42px;
+        height: 40px;
+        width: 40px;
       }
-      
-      .brand span {
-        display: inline-block;
+
+      .brand div {
+        font-weight: bold;
         vertical-align: middle;
-        height: 42px;
+        line-height: 40px;
+        margin-left: 10px;
       }
-      
+
       tweet-sidebar {
         display: inline-block;
         width: 10%;
       }
-    `
+    `;
   }
 
   render() {
@@ -44,12 +47,12 @@ export default class TweetHeader extends LitElement {
       <header>
         <tweet-sidebar></tweet-sidebar>
         <div class="brand">
-          <img src="./src/assets/images/tweetbook.png" alt="Tweetbook logo">
-          <span>Tweetbook</span>
+          <img src="./src/assets/images/tweetbook.png" alt="Tweetbook logo" />
+          <div>tweetbook</div>
         </div>
       </header>
-    `
+    `;
   }
 }
 
-customElements.define('tweet-header', TweetHeader);
+customElements.define("tweet-header", TweetHeader);
