7cd4c513ffec2725702bfe12d191da94c2a92eb1
Maxime AUBLET <maximeaublet@gmail.com>
2019-07-21 01:26:47 +0200

    :lipstick: tweet details

--- src/components/modules/InfosTweet.js
+++ src/components/modules/InfosTweet.js
@@ -1,142 +1,162 @@
-import {LitElement, html, css} from "lit-element";
-import './Tweet/Response';
-import './Tweet'
-import {EventConstant} from "../../Constants/event.constant";
+import { LitElement, html, css } from "lit-element";
+import "./Tweet/Response";
+import "./Tweet";
+import { EventConstant } from "../../Constants/event.constant";
 
 export class InfosTweet extends LitElement {
-    constructor() {
-        super();
-        this.tweet = {};
-        this.active = false;
-        this.responses = [];
-    }
+  constructor() {
+    super();
+    this.tweet = {};
+    this.active = false;
+    this.responses = [];
+  }
 
-    static get properties() {
-        return {
-            tweet: Object,
-            active: Boolean,
-            responses: [],
-        };
-    }
+  static get properties() {
+    return {
+      tweet: Object,
+      active: Boolean,
+      responses: []
+    };
+  }
 
-    firstUpdated(_changedProperties) {
-        document.addEventListener(EventConstant.DISPLAY_INFOS_TWEET, ({detail}) => this.displayInfosTweet(detail))
-        document.addEventListener(EventConstant.RESPONSE_TWEET_DONE, ({detail}) => {
-            if (this.tweet) {
-                this.responses = detail.responses;
-                this.tweet.data.responses = detail.responses;
-            }
-        })
-    }
+  firstUpdated(_changedProperties) {
+    document.addEventListener(EventConstant.DISPLAY_INFOS_TWEET, ({ detail }) =>
+      this.displayInfosTweet(detail)
+    );
+    document.addEventListener(
+      EventConstant.RESPONSE_TWEET_DONE,
+      ({ detail }) => {
+        if (this.tweet) {
+          this.responses = detail.responses;
+          this.tweet.data.responses = detail.responses;
+        }
+      }
+    );
+  }
 
-    render() {
-        return html`
-            <div class="infos-tweet">
-                <div class="sidebar ${this.active ? 'display' : ''}">
-                    <div class="header-sidebar">
-                        <h2>Tweet</h2>
-                        <button class="collapse-button" id="cross-icon" @click=${this.displaySidebar}></button>
-                    </div>
-                    <div class="tweet-container">
-                        <tweet-elem .tweet="${this.tweet}" noAction=true></tweet-elem>
-                        ${(this.responses.length > 0) ?
-            this.responses.map((item, key) => {
-                return html`<tweet-response .tweet="${item}" key=${key} last=${this.responses.length-1}></tweet-response>`
-            }) : html``}
-                    </div>
-                    <button id="respond" @click="${this.handleClick}">Respond</button>
-                <div>
-            </div>
-`;
-    }
+  render() {
+    return html`
+      <div class="infos-tweet">
+        <div class="sidebar ${this.active ? "display" : ""}">
+          <div class="header-sidebar">
+            <h2>Tweet</h2>
+            <button
+              class="collapse-button"
+              id="cross-icon"
+              @click=${this.displaySidebar}
+            ></button>
+          </div>
+          <div class="tweet-container">
+            <tweet-elem .tweet="${this.tweet}" noAction="true"></tweet-elem>
+            ${this.responses.length > 0
+              ? this.responses.map((item, key) => {
+                  return html`
+                    <tweet-response
+                      .tweet="${item}"
+                      key=${key}
+                      last=${this.responses.length - 1}
+                    ></tweet-response>
+                  `;
+                })
+              : html``}
+          </div>
+          <button id="respond" @click="${this.handleClick}">Respond</button>
+        </div>
+      </div>
+    `;
+  }
 
-    static get styles() {
-        // language=CSS
-        return css`
-            * {
-                box-sizing: border-box;
-            }
+  static get styles() {
+    // language=CSS
+    return css`
+      * {
+        box-sizing: border-box;
+      }
 
-            tweet-elem, tweet-response {
-                width: 100%;
-                display: block;
-            }
+      tweet-elem,
+      tweet-response {
+        width: 100%;
+        display: block;
+      }
 
-            .tweet-container {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
+      .tweet-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
 
-            .sidebar {
-                background-color: var(--app-bg-color);
-                width: 100%;
-                height: 100%;
-                top: 0;
-                bottom: 0;
-                right: -100%;
-                text-align: left;
-                position: fixed;
-                margin: 0;
-                padding: 0;
-                transition: 0.2s ease;
-                overflow: auto;
-                z-index: 10;
-            }
+      .sidebar {
+        background-color: var(--app-bg-color);
+        width: 100%;
+        height: 100%;
+        top: 0;
+        bottom: 0;
+        right: -100%;
+        text-align: left;
+        position: fixed;
+        margin: 0;
+        padding: 0;
+        transition: 0.2s ease;
+        overflow: auto;
+        z-index: 10;
+      }
 
-            .display {
-                right: 0 !important;
-                box-shadow: 2px 2px 30px 0px #656565;
-            }
+      .display {
+        right: 0 !important;
+        box-shadow: 2px 2px 30px 0px #656565;
+      }
 
-            .header-sidebar {
-                display: flex;
-                flex-direction: row-reverse;
-                align-items: center;
-                justify-content: space-between;
-                height: 48px;
-                border-bottom: solid var(--app-header-shadow) 1px;
-                box-shadow: var(--app-header-shadow) 0px 3px 4px 0px;
-                margin-bottom: 10px;
-                background-color: var(--app-bg-component-color);
-            }
+      .header-sidebar {
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+        justify-content: space-between;
+        height: 48px;
+        border-bottom: solid var(--app-header-shadow) 1px;
+        box-shadow: var(--app-header-shadow) 0px 3px 4px 0px;
+        margin-bottom: 10px;
+        background-color: var(--app-bg-component-color);
+      }
 
-            .header-sidebar > h2 {
-                width: 80%;
-            }
+      .header-sidebar > h2 {
+        width: 80%;
+      }
 
-            .collapse-button {
-                border: none;
-                height: 100%;
-                display: flex;
-                align-content: center;
-                justify-content: center;
-                width: 50px;
-                background: url("../../../src/assets/images/Back-Arrow-Icon-PNG-715x715.png") no-repeat center;
-                background-size: 100%;
-            }
-        `
-    }
+      .collapse-button {
+        border: none;
+        height: 100%;
+        display: flex;
+        align-content: center;
+        justify-content: center;
+        width: 50px;
+        background: url("../../../src/assets/images/Back-Arrow-Icon-PNG-715x715.png")
+          no-repeat center;
+        background-size: 100%;
+      }
+    `;
+  }
 
-    displaySidebar() {
-        this.active = !this.active;
-        this.tweet = {};
-    }
+  displaySidebar() {
+    this.active = !this.active;
+    this.tweet = {};
+  }
 
-    displayInfosTweet(tweet) {
-        this.active = true;
-        this.tweet = tweet;
-        this.responses = tweet.data.responses;
-    }
+  displayInfosTweet(tweet) {
+    this.active = true;
+    this.tweet = tweet;
+    this.responses = tweet.data.responses;
+  }
 
-    handleClick() {
-        document.dispatchEvent(new CustomEvent(EventConstant.RESPONSE, {
-            detail: {
-                tweet: this.tweet,
-                dontNeedDisplay: true
-            }
-        }))
-    }
+  handleClick() {
+    document.dispatchEvent(
+      new CustomEvent(EventConstant.RESPONSE, {
+        detail: {
+          tweet: this.tweet,
+          dontNeedDisplay: true
+        }
+      })
+    );
+  }
 }
 
 customElements.define("infos-tweet", InfosTweet);
--- src/components/modules/Tweet/Response.js
+++ src/components/modules/Tweet/Response.js
@@ -1,35 +1,34 @@
-import {LitElement, html, css} from "lit-element";
-import {EventConstant} from "../../../Constants/event.constant";
-import lozad from 'lozad';
+import { LitElement, html, css } from "lit-element";
+import { EventConstant } from "../../../Constants/event.constant";
+import lozad from "lozad";
 
 export class TweetResponse extends LitElement {
-    constructor() {
-        super();
-        this.tweet = {};
-        this.key = 0;
-        this.last = 0;
-    }
+  constructor() {
+    super();
+    this.tweet = {};
+    this.key = 0;
+    this.last = 0;
+  }
 
-    static get properties() {
-        return {
-            tweet: Object,
-            key: Number,
-            last: Number
-        };
-    
-    }
+  static get properties() {
+    return {
+      tweet: Object,
+      key: Number,
+      last: Number
+    };
+  }
 
-    firstUpdated(_changedProperties) {
-      const observer = lozad(this.shadowRoot.querySelectorAll('.lozad'), {
-        load: function(el) {
-            console.info('loading element');
-        }
-      });
-      observer.observe();
-    }
+  firstUpdated(_changedProperties) {
+    const observer = lozad(this.shadowRoot.querySelectorAll(".lozad"), {
+      load: function(el) {
+        console.info("loading element");
+      }
+    });
+    observer.observe();
+  }
 
-    static get styles() {
-        return css`
+  static get styles() {
+    return css`
       .tweet {
         min-height: 100px;
         display: flex;
@@ -38,26 +37,26 @@ export class TweetResponse extends LitElement {
         position: relative;
       }
 
-       .divider {
+      .divider {
         height: 100%;
         width: 2px;
-        background: #cecece;
+        background: var(--app-divider);
         z-index: 1000;
         display: block;
-        position:absolute;
+        position: absolute;
         left: 50%;
         transform: translateX(-50%);
         z-index: 10;
-       }
-       
-       .divider-first {
-            top: 50%;
-            height: 50%;
-       }
-       
-       .divider-last {
-           height: 50%;
-       }
+      }
+
+      .divider-first {
+        top: 50%;
+        height: 50%;
+      }
+
+      .divider-last {
+        height: 50%;
+      }
 
       .user-pic-box {
         width: 100px;
@@ -70,18 +69,18 @@ export class TweetResponse extends LitElement {
       .user-pic {
         background-size: 70px 70px;
         border-radius: 50%;
-        border: 3px solid white;
+        border: 3px solid var(--app-bg-component-color);
         height: 70px;
         width: 70px;
         background-color: var(--app-bg-color);
         z-index: 11;
       }
-      
-      .center-absolute{
+
+      .center-absolute {
         position: absolute;
         top: 50%;
         left: 50%;
-        transform: translate(-50%,-50%);
+        transform: translate(-50%, -50%);
       }
 
       .content {
@@ -109,12 +108,12 @@ export class TweetResponse extends LitElement {
       .user-tn {
         font-weight: bold;
       }
-        
-        .date {
-            text-align: right;
-            font-size: 14px;
-            color: #5a5a5a;
-        }
+
+      .date {
+        text-align: right;
+        font-size: 14px;
+        color: #5a5a5a;
+      }
 
       .user-at {
         color: var(--app-color-link);
@@ -125,19 +124,23 @@ export class TweetResponse extends LitElement {
       .tweet-content {
         width: 85%;
       }
-      
+
       button-action {
         width: 210px;
       }
     `;
-    }
+  }
 
-    render() {
-        const date = new Date(this.tweet.date);
-        return html`
-      <div class="tweet" >
+  render() {
+    const date = new Date(this.tweet.date);
+    return html`
+      <div class="tweet">
         <div class="user-pic-box">
-        <div class="divider ${this.last === this.key ? "divider-last" : ""} ${this.key == 0 ? "divider-first" : ""}"></div>
+          <div
+            class="divider ${this.last === this.key
+              ? "divider-last"
+              : ""} ${this.key == 0 ? "divider-first" : ""}"
+          ></div>
           <div
             class="user-pic lozad center-absolute"
             style="background-image: url(${this.tweet.user.loadedAvatar});"
@@ -149,12 +152,17 @@ export class TweetResponse extends LitElement {
             <div class="user-info-box">
               <div class="user-info">
                 <span class="user-tn"
-                  >${this.tweet.user.name +
-        " " +
-        this.tweet.user.surname}
+                  >${this.tweet.user.name + " " + this.tweet.user.surname}
                 </span>
-                <a href="#"><span class="user-at">${" @" + this.tweet.user.nickname}</span></a>
-              <span class="date"> - ${date.toLocaleDateString()} ${date.toLocaleTimeString()}</span>
+                <a href="#" style="text-decoration:none;"
+                  ><span class="user-at"
+                    >${" @" + this.tweet.user.nickname}</span
+                  ></a
+                >
+                <span class="date">
+                  - ${date.toLocaleDateString()}
+                  ${date.toLocaleTimeString()}</span
+                >
               </div>
               <div class="delete" @click="${e => this.deleteTweet(e)}">
                 <i>X</i>
@@ -165,13 +173,15 @@ export class TweetResponse extends LitElement {
         </div>
       </div>
     `;
-    }
+  }
 
-    deleteTweet(e) {
-        document.dispatchEvent(
-            new CustomEvent(EventConstant.DELETE_TWEET_RESPONSE, {detail: this.tweet})
-        );
-    }
+  deleteTweet(e) {
+    document.dispatchEvent(
+      new CustomEvent(EventConstant.DELETE_TWEET_RESPONSE, {
+        detail: this.tweet
+      })
+    );
+  }
 }
 
 customElements.define("tweet-response", TweetResponse);
